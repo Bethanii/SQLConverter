dc01dcc9a1bcae7bbe240b53b837fff40c65fc8c
Bethany Hampton <bethanihampton@gmail.com>
2023-12-11 13:54:08 -0600

    added login functionality

--- SQLConverter/src/main/java/com/example/myjavafxapp/Controller.java
+++ SQLConverter/src/main/java/com/example/myjavafxapp/Controller.java
@@ -4,21 +4,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Controller {
 
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField emailInputField;
+    @FXML
+    private TextField passwordInputField;
     private AnchorPane signInPage;
 
+
     @FXML
-    private void DatabaseConnection() throws IOException {
+    private Connection DatabaseConnection() throws IOException {
         try {
             String url = "jdbc:mysql://127.0.0.1:3306/SQLConverter?user=username&password=password";
             String username = "username";
@@ -26,14 +30,16 @@ public class Controller {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            try (Connection dbConnection = DriverManager.getConnection(url, username, password)) {
+            Connection dbConnection = DriverManager.getConnection(url, username, password);
                 if (dbConnection != null) {
                     System.out.println("Successfully connected to MySQL database SQLConverter");
+
+                    return dbConnection;
                 }
-            }
         } catch (SQLException | ClassNotFoundException ex) {
             System.out.println("An error occurred while connecting to the MySQL database");
         }
+        return null;
     }
 
     @FXML
@@ -44,6 +50,7 @@ public class Controller {
         signInPage = fxmlLoader.load();
         Scene currentScene = welcomeText.getScene();
         currentScene.setRoot(signInPage);
+        signInPage.requestFocus();
 
         Stage stage = (Stage) currentScene.getWindow();
         stage.sizeToScene();
@@ -57,6 +64,50 @@ public class Controller {
 
     @FXML
     protected void onSignInPageButtonClick() throws IOException {
-        DatabaseConnection();
+        validateUserLogin();
+    }
+
+    @FXML
+    private String getUserInput(TextField userInputField) throws IOException {
+        String userInput = userInputField.getText();
+        System.out.println("User Input: " + userInput);
+
+        return userInput;
+    }
+
+    @FXML
+    private void displayLoginValidationError () throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(SQLApplication.class.getResource("login-validation-error.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 820, 500);
+        stage.setTitle("Login Validation Error");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void validateUserLogin() throws IOException {
+        String userEmail = getUserInput(emailInputField);
+        String userPassword = getUserInput(passwordInputField);
+
+        Connection connection = DatabaseConnection();
+         try
+         {
+             String sql = "SELECT * FROM users WHERE email = ?";
+             try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                 statement.setString(1, userEmail);
+
+                 try (ResultSet resultSet = statement.executeQuery()) {
+                     if (resultSet.next()) {
+                         System.out.println("User exists in the database.");
+                     } else {
+                         System.out.println("User does not exist in the database.");
+                         displayLoginValidationError();
+                     }
+                 }
+             }
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
     }
 }
--- SQLConverter/src/main/java/com/example/myjavafxapp/SQLApplication.java
+++ SQLConverter/src/main/java/com/example/myjavafxapp/SQLApplication.java
@@ -1,17 +1,18 @@
 package com.example.myjavafxapp;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class SQLApplication extends Application {
+
+    @FXML
+    private AnchorPane landingPage;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SQLApplication.class.getResource("landing-page.fxml"));
@@ -19,7 +20,10 @@ public class SQLApplication extends Application {
         stage.setTitle("SQL Converter");
         stage.setScene(scene);
         stage.show();
+        stage.setOnShown(event -> landingPage.requestFocus());
+        stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
