9fb42236a8a9db5b1af9e048fa3c781349022e23
bethanii <bethanihampton@gmail.com>
2024-03-17 17:39:01 -0500

    adding dynamic rtow functionality

--- SQLConverter/src/main/java/com/example/myjavafxapp/SQLConverterController.java
+++ SQLConverter/src/main/java/com/example/myjavafxapp/SQLConverterController.java
@@ -4,10 +4,14 @@ import javafx.fxml.FXML;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.util.Collections;
 import java.util.List;
 import javafx.scene.control.ChoiceBox;
-
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 
 public class SQLConverterController
 {
@@ -15,33 +19,33 @@ public class SQLConverterController
     private ChoiceBox searchRow1;
     @FXML
     private TextField searchField1;
+    @FXML
+    private VBox dynamicRowsContainer;
+    @FXML
+    private VBox resultsContainer;
+
+    private ChoiceBox<String> activeChoiceBox;
 
     @FXML
-    protected void onConnectButtonClick() throws IOException
-    {
-        ConnectToDB();
+    public void initialize() {
+        searchRow1.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                activeChoiceBox = searchRow1;
+            }
+        });
     }
-
     @FXML
     protected void onSearchButtonClick() throws IOException
     {
+        if (activeChoiceBox == null || activeChoiceBox.getValue() == null) {
+            System.out.println("Please select a table first.");
+            return;
+        }
+
         String searchKeyword = searchField1.getText();
-        String dropdownSelection = (String) searchRow1.getValue();
+        String dropdownSelection = activeChoiceBox.getValue();
 
-        querySelectedTables(dropdownSelection, searchKeyword);
-    }
-    protected void ConnectToDB() throws IOException
-    {
-        String serverName = "sqlservertest-db.database.windows.net";
-        String databaseName = "SQLServer_TestDB";
-        String dbUsername = "TestUser";
-        String dbPassword = "SQLservertest1!";
-
-        DatabaseManager databaseManager = new DatabaseManager();
-        Connection connection = databaseManager.ConnectUserDatabase(serverName, databaseName, dbUsername, dbPassword);
-
-        List<String> tableNames = getTableNames(connection);
-        searchRow1.getItems().addAll(tableNames);
+        querySelectedTables(dropdownSelection, Collections.singletonList(searchKeyword));
     }
 
     public List<String> getTableNames(Connection connection)
@@ -85,52 +89,90 @@ public class SQLConverterController
         return columnNames;
     }
 
-    private void querySelectedTables(String tableName, String searchText) throws IOException
-    {
+    private void querySelectedTables(String tableName, List<String> searchTexts) {
         String serverName = "sqlservertest-db.database.windows.net";
         String databaseName = "SQLServer_TestDB";
         String dbUsername = "TestUser";
         String dbPassword = "SQLservertest1!";
 
         DatabaseManager databaseManager = new DatabaseManager();
+        List<String> results = new ArrayList<>();
 
-        try (Connection connection = databaseManager.ConnectUserDatabase(serverName, databaseName, dbUsername, dbPassword))
-        {
+        try (Connection connection = databaseManager.ConnectUserDatabase(serverName, databaseName, dbUsername, dbPassword)) {
             List<String> columnNames = getColumnNames(connection, tableName);
 
-            StringBuilder whereClause = new StringBuilder();
             for (String columnName : columnNames) {
-                if (whereClause.length() > 0) {
-                    whereClause.append(" OR ");
-                }
-                whereClause.append(columnName).append(" LIKE ?");
-            }
+                String query = "SELECT * FROM " + tableName + " WHERE " + columnName + " LIKE ?";
+                try (PreparedStatement statement = connection.prepareStatement(query)) {
+                    for (String searchText : searchTexts) {
+                        statement.setString(1, "%" + searchText + "%");
 
-            String query = "SELECT * FROM " + tableName + (whereClause.length() > 0 ? " WHERE " + whereClause.toString() : "");
-
-            try (PreparedStatement statement = connection.prepareStatement(query))
-            {
-                for (int i = 1; i <= columnNames.size(); i++)
-                {
-                    statement.setString(i, "%" + searchText + "%");
-                }
-                try (ResultSet resultSet = statement.executeQuery())
-                {
-                    while (resultSet.next())
-                    {
-                        for (int i = 1; i <= columnNames.size(); i++)
-                        {
-                            if (i > 1) System.out.print(",  ");
-                            String columnValue = resultSet.getString(i);
-                            System.out.print(columnNames.get(i - 1) + ": " + columnValue);
+                        try (ResultSet resultSet = statement.executeQuery()) {
+                            while (resultSet.next()) {
+                                StringBuilder row = new StringBuilder();
+                                ResultSetMetaData rsmd = resultSet.getMetaData();
+                                int columnCount = rsmd.getColumnCount();
+                                for (int i = 1; i <= columnCount; i++) {
+                                    if (i > 1) row.append(", ");
+                                    row.append(rsmd.getColumnName(i)).append(": ").append(resultSet.getString(i));
+                                }
+                                results.add(row.toString());
+                            }
                         }
-                        System.out.println();
                     }
                 }
             }
-        } catch (SQLException e) {
+            displayQueryResults(results);
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void displayQueryResults(List<String> results) {
+        resultsContainer.getChildren().clear();
+
+        for (String row : results) {
+            Label rowLabel = new Label(row);
+            rowLabel.setStyle("-fx-padding: 5;");
+            resultsContainer.getChildren().add(rowLabel);
+        }
+    }
+    
+    @FXML
+    protected void onAddRowButtonClick() {
+        ChoiceBox<String> newChoiceBox = new ChoiceBox<>();
+        newChoiceBox.setPrefWidth(167.0);
+        newChoiceBox.setPrefHeight(33.0);
+
+        newChoiceBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                activeChoiceBox = newChoiceBox;
+            }
+        });
+
+        DatabaseManager databaseManager = new DatabaseManager();
+        try {
+            Connection connection = databaseManager.ConnectUserDatabase("sqlservertest-db.database.windows.net", "SQLServer_TestDB", "TestUser", "SQLservertest1!");
+            newChoiceBox.getItems().addAll(getTableNames(connection));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TextField newTextField = new TextField();
+        newTextField.setPrefWidth(250.0);
+        newTextField.setPrefHeight(33.0);
+        newTextField.setPromptText("Enter value");
+
+        Button searchButton = new Button("Search");
+        searchButton.setOnAction(event -> {
+            String selectedTable = newChoiceBox.getValue();
+            String searchText = newTextField.getText();
+            querySelectedTables(selectedTable, Collections.singletonList(searchText));
+        });
+
+        HBox newRow = new HBox(10);
+        newRow.getChildren().addAll(newChoiceBox, newTextField, searchButton);
+
+        dynamicRowsContainer.getChildren().add(newRow);
+    }
 }
