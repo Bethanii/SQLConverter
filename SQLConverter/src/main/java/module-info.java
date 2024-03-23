49db76c4ac1c7658cbf8104eed6a62c942519347
bethanii <bethanihampton@gmail.com>
2024-03-22 19:14:08 -0500

    setting up email

--- SQLConverter/src/main/java/module-info.java
+++ SQLConverter/src/main/java/module-info.java
@@ -6,13 +6,16 @@ module com.example.myjavafxapp {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
-    requires java.mail;
     requires javax.mail.api;
     requires com.google.api.client;
     requires com.google.api.client.extensions.jetty.auth;
     requires google.api.client;
     requires com.google.api.client.auth;
     requires com.google.api.client.extensions.java6.auth;
+    requires com.google.api.services.gmail;
+    requires org.apache.commons.codec;
+    requires com.google.api.client.json.gson;
+
 
     opens com.example.myjavafxapp to javafx.fxml;
     exports com.example.myjavafxapp;
